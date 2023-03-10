1d9aa5d73d83d72107bd6eafe7d189c31bb1b3d7
emon3455 <emon.mhk69@gmail.com>
2023-03-10 23:53:20 +0600

    ranga store fixed successfully

--- rangaStore/js/app.js
+++ rangaStore/js/app.js
@@ -18,15 +18,16 @@ const showProducts = (products) => {
 
    document.getElementById("all-products").innerHTML = "";
 
-   const allProducts = products.slice(0, 10).map((pd) => pd);
+   const allProducts = products.slice(0, 10);
    for (const product of allProducts) {
-      const image = product.images;
+      const image = product.image;
       const div = document.createElement('div');
       div.classList.add('product');
-      div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
+      div.innerHTML = `
+      <div class="single-product">
+         <div>
+            <img class="product-image" src=${image}></img>
+         </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
@@ -44,61 +45,65 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
-
+   updatePrice('price', price);
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
+   updateTotal();
 };
 
 const showProductDetails = (product_id) => {
    console.log(product_id);
    fetch(`https://fakestoreapi.com/products/${product_id}`)
-      .then((res) => res.json())
-      .then((data) => showProductDetailsInModal(data));
+      .then(res => res.json())
+      .then(data => showProductDetailsInModal(data));
 };
 
 const showProductDetailsInModal = (product_details) => {
    console.log(product_details);
    setInnerText('exampleModalLabel', product_details.title);
-   setInnerText('product_id', product_details.id);
+   setInnerText('productId', product_details.id);
    setInnerText('modal_body', product_details.description);
    setInnerText('rating', product_details.rating.rate);
 };
 
 const getInputValue = (id) => {
    const element = document.getElementById(id).innerText;
-   const converted = parseInt(element);
+   const converted = parseFloat(element);
    return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total.toFixed(2);
+   updateTotal();
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-   document.getElementById(id).innerText = Math.round(value);
+   document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
+
    const priceConverted = getInputValue('price');
-   if (priceConverted > 200) {
-      setInnerText('delivery-charge', 30);
-      setInnerText('total-tax', priceConverted * 0.2);
-   }
-   if (priceConverted > 400) {
-      setInnerText('delivery-charge', 50);
-      setInnerText('total-tax', priceConverted * 0.3);
-   }
+
    if (priceConverted > 500) {
       setInnerText('delivery-charge', 60);
-      setInnerText('total-tax', priceConverted * 0.4);
+      setInnerText('total-tax', (priceConverted * 0.4).toFixed(2));
    }
+   else if(priceConverted > 400) {
+      setInnerText('delivery-charge', 50);
+      setInnerText('total-tax', (priceConverted * 0.3).toFixed(2));
+   }
+   else if(priceConverted > 200) {
+      setInnerText('delivery-charge', 30);
+      setInnerText('total-tax', (priceConverted * 0.2).toFixed(2));
+   }
+   
 };
 
 //grandTotal update function
@@ -107,16 +112,17 @@ const updateTotal = () => {
       getInputValue('price') +
       getInputValue('delivery-charge') +
       getInputValue('total-tax');
-   document.getElementById('total').innerText = grandTotal;
+   document.getElementById('total').innerText = grandTotal.toFixed(2);
 };
 
 // search by category
 document.getElementById("search-btn").addEventListener("click", function () {
    const inputField = document.getElementById("input-value").value;
-   const searchedProduct = arr[0].find((p) =>
-     p.category.startsWith(`${inputField}`)
+   const searchedProduct = arr[0].filter((p) =>
+     (p.title.toLowerCase()).includes(`${inputField.toLowerCase()}`)
    );
    showProducts(searchedProduct);
+   document.getElementById("input-value").value="";
  });
 
 
