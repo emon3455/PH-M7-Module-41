34381c379f812de33427d10e0254ab846e3e5536
emon3455 <emon.mhk69@gmail.com>
2023-03-10 22:49:53 +0600

    bap ar bank fixed

--- bank/js/deposit.js
+++ bank/js/deposit.js
@@ -13,13 +13,13 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const previousDepositTotal = getTextElementValueById("deposit-total");
 
   // calculate new deposit total
-  const newDepositTotal = previousDepositTotal + newDepositAmount;
   console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
   // set deposit total value
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
+  const previousBalanceTotal = getTextElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal + newDepositAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
-);
+
+});
